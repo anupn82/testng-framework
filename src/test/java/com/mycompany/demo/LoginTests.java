6f101b1d78add40e0ea81ee8773dfbe8f1db396b
anup narayanan <anup.n82@gmail.com>
2020-09-26 20:24:20 -0400

    Included Assertions - Assert.assertEquals, Assert.assertTrue, Assert.fail. Closes #3

--- src/test/java/com/mycompany/demo/LoginTests.java
+++ src/test/java/com/mycompany/demo/LoginTests.java
@@ -14,10 +14,14 @@ public class LoginTests {
     @Test(priority = 2, testName = "Login test with invalid username and password")
     public void loginWithInvalidUser(){
         System.out.println("Login test with invalid User");
+        Assert.fail("Invalid user login test failed");
     }
     @Test(priority = 3, testName = "Verify home page title")
     public void homePageTest(){
         System.out.println("This is a homepage test");
+        String exp = "Homepage";
+        String act = "Homepage";
+        Assert.assertTrue(exp==act, "Home page login not valid");
     }
     @Test(priority = 4, testName = "Verify Logout")
     public void logoutTest(){
